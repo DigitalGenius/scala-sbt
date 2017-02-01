d7424dcbd9a7fa4929b6afd3ac2635c4d9660a53
Mathias Bogaert <mbogaert@nephilaadvisors.co.uk>
2017-02-01 22:02:26 +0000

    Add Scala 2.11.8.

--- test-sbt.sh
+++ test-sbt.sh
@@ -7,6 +7,15 @@
 #
 
 cat << EOF | sbt
+set name := "SbtTest211"
+set version := "1.0"
+set scalaVersion := "2.11.8"
+session save
+compile
+exit
+EOF
+
+cat << EOF | sbt
 set name := "SbtTest"
 set version := "1.0"
 set scalaVersion := "$SCALA_VERSION"
